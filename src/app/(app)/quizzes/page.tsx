e0e8a7543ccba7b626d4a0b0ba50f2b036d4f955
Abhinaba Roy Pradhan <abhinabapradhan@gmail.com>
2025-11-07 19:45:27 +0000

    in number of questions section give max to 50  also give another categoy

--- src/app/(app)/quizzes/page.tsx
+++ src/app/(app)/quizzes/page.tsx
@@ -165,6 +165,10 @@ export default function QuizzesPage() {
                             <SelectItem value="10">10</SelectItem>
                             <SelectItem value="15">15</SelectItem>
                             <SelectItem value="20">20</SelectItem>
+                            <SelectItem value="25">25</SelectItem>
+                            <SelectItem value="30">30</SelectItem>
+                            <SelectItem value="40">40</SelectItem>
+                            <SelectItem value="50">50</SelectItem>
                         </SelectContent>
                     </Select>
                 </div>
